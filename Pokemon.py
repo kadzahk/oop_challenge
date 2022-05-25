a50dc14d6393ad585e4284c6ec594ec7853c875c
kadzahk <juan427lol@gmail.com>
2022-05-25 14:14:29 -0500

    we did it >:D

--- Pokemon.py
+++ Pokemon.py
@@ -53,9 +53,9 @@ class pokemon:
         while (self.health > 0) and (other.health > 0):
             print(self.name, " is attacking to ", other.name)
             
-            rng_damage = self.attack * random.randinit(5)
-            other.health -= rng_damage
-            print(self.name, " did ", rng_damage, " to ", other.name)
+            """ rng_damage = self.attack * random.randinit(5) """
+            other.health -= self.attack
+            print(self.name, " did ", self.attack, " to ", other.name)
             
             print("the health of ", other.name, " has been reduced to:", other.health)
 
@@ -66,9 +66,9 @@ class pokemon:
 
             print(other.name, " is attacking to ", self.name)
            
-            rng_damage = other.attack * random.randinit(5)
-            self.health -= rng_damage
-            print(other.name, " did ", rng_damage, " to ", self.name)
+            """ rng_damage = other.attack * random.randinit(5) """
+            self.health -= other.attack
+            print(other.name, " did ", other.attack, " to ", self.name)
             
             print("the health of ", self.name, " has been reduced to:", self.health)
 
@@ -77,10 +77,10 @@ class pokemon:
                 break
 
 if __name__ == '__main__':
-    piph = input("insert the attack amount between 5 or 1 \n")
+    piph = int(input("insert the attack amount between 5 or 1 \n"))
     piplup = pokemon("Piplup", 389, 0.4, 5.2, piph, "Water", "Blue", 20)
 
-    chart = input("insert the attack amount between 5 or 1 \n")
+    chart = int(input("insert the attack amount between 5 or 1 \n"))
     charmander = pokemon("Charmander", 4, 0.6, 8.5, chart, "Fire", "Red", 20)
 
     piplup.showdown(charmander)
