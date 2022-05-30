10d412fcf44f24b3e36e1b0c9567d3a698fa19ff
kadzahk <juan427lol@gmail.com>
2022-05-30 16:48:18 -0500

    rng added

--- Pokemon.py
+++ Pokemon.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
-from numpy import random
+import random
+import time
 
 
 class pokemon:
@@ -15,20 +16,11 @@ class pokemon:
         self.color = color
         self.health = health
 
-    def showpoints(self, other):
-        while (self.health > 0) and (other.health > 0):
-            # imprimir los puntos de salud de cada pokemon
-            print(f"\n{self.name}\tPoints of health\t{self.health}")
-            print(f"\n{other.name}\tPoints of health\t{other.health}")
-
-        self.attack(other)
-        self.printP1()
-        self.printP2(other)
-
 
     def showdown(self, other):
 
         print("-----POKEMONE BATTLE-----")
+        time.sleep(1.5)
 
         print("Name: ", self.name)
         print("Number: ", self.number)
@@ -38,9 +30,9 @@ class pokemon:
         print("Attack: ", self.attack)
         print("Weight: ", self.weight)
         print("HP: ", self.health)
-
+        time.sleep(1.5)
         print("-----VS!!!-----")
-
+        time.sleep(1.5)
         print("Name: ", other.name)
         print("Type: ", other.typeP)
         print("Color: ", other.color)
@@ -51,36 +43,41 @@ class pokemon:
         print("Height: ", other.height)
 
         while (self.health > 0) and (other.health > 0):
+            time.sleep(1.5)
             print(self.name, " is attacking to ", other.name)
-            
-            """ rng_damage = self.attack * random.randinit(5) """
-            other.health -= self.attack
-            print(self.name, " did ", self.attack, " to ", other.name)
-            
+            time.sleep(1.5)
+            rng_damage1 = self.attack * random.randint(0,5)
+            other.health -= rng_damage1
+            print(self.name, " did ", rng_damage1, " to ", other.name)
+            time.sleep(1.5)
             print("the health of ", other.name, " has been reduced to:", other.health)
 
             if other.health <= 0:
+                time.sleep(2)
                 print("\n..." + other.name + ' fainted. :(')
                 break
+            time.sleep(1.5)
             print("now is the turn of ", other.name)
-
+            time.sleep(1.5)
             print(other.name, " is attacking to ", self.name)
            
-            """ rng_damage = other.attack * random.randinit(5) """
-            self.health -= other.attack
-            print(other.name, " did ", other.attack, " to ", self.name)
-            
+            rng_damage2 = other.attack * random.randint(0,5)
+            self.health -= rng_damage2
+            time.sleep(1.5)
+            print(other.name, " did ", rng_damage2, " to ", self.name)
+            time.sleep(1.5)
             print("the health of ", self.name, " has been reduced to:", self.health)
 
             if self.health <= 0:
+                time.sleep(2)
                 print("\n..." + self.name + ' fainted. :(')
                 break
 
 if __name__ == '__main__':
-    piph = int(input("insert the attack amount between 5 or 1 \n"))
+    piph = int(input("insert the attack amount of piplup between 5 or 1 \n"))
     piplup = pokemon("Piplup", 389, 0.4, 5.2, piph, "Water", "Blue", 20)
 
-    chart = int(input("insert the attack amount between 5 or 1 \n"))
+    chart = int(input("insert the attack amount of charmander between 5 or 1 \n"))
     charmander = pokemon("Charmander", 4, 0.6, 8.5, chart, "Fire", "Red", 20)
 
     piplup.showdown(charmander)
